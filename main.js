145523838b0b95f3abe9cfedfa0ad8370b6c454d
Saturngod <saturngod@gmail.com>
2015-09-14 15:28:25 +0800

    fixed save as location if missing epub ext

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  //if (process.platform != 'darwin') {
     app.quit();
-  }
+    //}
 });
 
 // This method will be called when Electron has finished
@@ -31,6 +31,7 @@ app.on('ready', function() {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
+    console.log('windows close');
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
